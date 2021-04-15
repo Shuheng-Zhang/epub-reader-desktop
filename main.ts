1e275c111fdc6ca542171b0629b3cfa273f634db
Shuheng-Zhang <zhangshuheng01@163.com>
2021-04-16 03:25:23 +0800

    修正 应用无法启动的问题

--- main.ts
+++ main.ts
@@ -8,15 +8,17 @@ const args = process.argv.slice(1),
 
 function createWindow(): BrowserWindow {
 
-  const electronScreen = screen;
-  const size = electronScreen.getPrimaryDisplay().workAreaSize;
+  // const electronScreen = screen;
+  // const size = electronScreen.getPrimaryDisplay().workAreaSize;
 
   // Create the browser window.
   win = new BrowserWindow({
     x: 0,
     y: 0,
-    width: size.width,
-    height: size.height,
+    width: 1280,
+    height: 900,
+    minWidth: 1280,
+    minHeight: 900,
     webPreferences: {
       nodeIntegration: true,
       allowRunningInsecureContent: (serve) ? true : false,
